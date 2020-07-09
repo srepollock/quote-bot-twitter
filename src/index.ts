0f9c6546cd2164d97f41867201efb721228c1c2b
Spencer Pollock <spencer@spollock.ca>
2020-07-09 13:43:32 -0700

    chore: cleanup the merged files

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * A simple function to add two numbers together and return the sum.
- * @param  {number} x first number
- * @param  {number} y second number
- * @returns number sum
- */
-export function add(x: number, y: number): number {
-    return x + y;
-}
